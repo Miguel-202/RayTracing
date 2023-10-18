154f8f87d2056ff2854676006941265e6d92994b
Miguel-202 <mikytinez@hotmail.com>
2023-10-18 15:54:56 -0400

    Created rendering viewport
    Now application can render an array of pixels using the CPU.

--- RayTracingRenderer/src/WalnutApp.cpp
+++ RayTracingRenderer/src/WalnutApp.cpp
@@ -2,18 +2,59 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("Settings");
+		ImGui::Text("Last Render: %.3fms", m_lastRenderTimer);
+		if (ImGui::Button("Render"))
+		{
+			Render();
+		}
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("Viewport");
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y; 
+
+		if (m_Image)
+		{
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+		}
+		ImGui::End();
+		ImGui::PopStyleVar();
 	}
+
+	void Render()
+	{
+		Walnut::Timer timer;
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Walnut::Image>(m_ViewportWidth, m_ViewportHeight, Walnut::ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportHeight * m_ViewportWidth; i++)
+		{
+			m_ImageData[i] = Walnut::Random::UInt();
+			m_ImageData[i] |= 0xff000000; // Alpha = 255
+		}
+		m_Image->SetData(m_ImageData);
+		m_lastRenderTimer = timer.ElapsedMillis();
+	}
+private:
+	std::shared_ptr<Walnut::Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
+	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+
+	float m_lastRenderTimer = 0.0f;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
