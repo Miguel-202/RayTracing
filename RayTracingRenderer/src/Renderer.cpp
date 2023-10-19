821972b7cfa994963122b6f4a5cec9d78e29bfc3
Miguel-202 <mikytinez@hotmail.com>
2023-10-19 17:48:50 -0400

    Add camera movement and refactor renderer for camera integration
    This commit introduces two major changes:
    
    1. Camera Movement:
       - Added the ability for the camera to move using keyboard and mouse input.
       - Implemented camera zoom and pan functionalities.
    
    2. Renderer Refactor:
       - Modified the Renderer class to accept a Camera object.
       - Refactored the `Render()` function to use camera parameters like position and orientation.
    
    These changes improve the flexibility and modularity of the rendering pipeline, and enhance the user interaction experience by enabling dynamic camera movement.

--- RayTracingRenderer/src/Renderer.cpp
+++ RayTracingRenderer/src/Renderer.cpp
@@ -20,7 +20,7 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	if (m_FinalImage)
 	{
 		//No resize necessary
-		if (m_FinalImage->GetWidth() != width || m_FinalImage->GetHeight() != height)
+		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
 			return; 
 		m_FinalImage->Resize(width, height);
 	}
@@ -33,16 +33,21 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	m_ImageData = new uint32_t[width * height];
 }
 
-void Renderer::Render()
+void Renderer::Render(const Camera& camera)
 {
+	Ray ray;
+	ray.Origin = camera.GetPosition();;
+
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
-			glm::vec2 coord = { x / (float)m_FinalImage->GetWidth(),  y / (float)m_FinalImage->GetHeight()};
-			coord = coord * 2.0f - 1.0f;
+			size_t index = x + y * m_FinalImage->GetWidth();
+			if (camera.GetRayDirections().size() <= index)
+				continue;
+			ray.Direction =  camera.GetRayDirections()[index];
 
-			glm::vec4 color = PerPixel(coord);
+			glm::vec4 color = TraceRay(ray);
 			color = glm::clamp(color, 0.0f, 1.0f);
 			m_ImageData[y * m_FinalImage->GetWidth() + x] = Utils::ConvertToRGBA(color);
 		}
@@ -50,24 +55,17 @@ void Renderer::Render()
 	m_FinalImage->SetData(m_ImageData);
 }
 
-glm::vec4 Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::TraceRay(const Ray& ray)
 {
-	uint8_t r = (uint8_t)(coord.x * 255.0f);
-	uint8_t g = (uint8_t)(coord.y * 255.0f);
-
-	glm::vec3 rayOrigin = { 0.0f, 0.0f, 2.0f };
-	glm::vec3 rayDirection = { coord.x, coord.y, -1.0f };
-	//rayDirection = glm::normalize(rayDirection);
 	float radius = 0.5f;
-
 	//(bx^2 + by^2)t^2 + 2(abxb + ayby)t + (ax^2 + ay^2 - r^2) = 0
 	//a = (bx^2 + by^2) = Ray Origin
 	//b = (ax^2 + ay^2 - r^2) = Ray Direction
 	//r = radius
 	//t = (-b +- sqrt(b^2 - 4ac)) / 2a = Intersection or Hit Distance
-	float a = glm::dot(rayDirection, rayDirection);
-	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
-	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+	float a = glm::dot(ray.Direction, ray.Direction);
+	float b = 2.0f * glm::dot(ray.Origin, ray.Direction);
+	float c = glm::dot(ray.Origin, ray.Origin) - radius * radius;
 
 	// Discriminant = b^2 - 4ac
 	float discriminant = b * b - 4.0f * a * c;
@@ -81,15 +79,15 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 			(-b - sqrt(discriminant)) / (2.0f * a),
 			(-b + sqrt(discriminant)) / (2.0f * a)
 		};
-		glm::vec3 lightDir = glm::normalize(glm::vec3(-2.0f, -2.0f, 2.0f)); 
 
 		for (int i = 0; i < 1; i++)
 		{
-			if (t[i] > 0.0f)
+			//if (t[i] > 0.0f)
 			{
-				glm::vec3 hitPoint = rayOrigin + rayDirection * t[i];
-				glm::vec3 normal = hitPoint - rayOrigin;
-				normal = glm::normalize(normal);
+				glm::vec3 hitPoint = ray.Origin + ray.Direction * t[0];
+				glm::vec3 normal = glm::normalize(hitPoint);
+
+				glm::vec3 lightDir = glm::normalize(glm::vec3(-2.0f, -2.0f, 2.0f));
 
 				float intensity =  glm::dot(normal, -lightDir);
 				intensity = glm::max(intensity, 0.0f);
